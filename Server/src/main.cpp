70d6323c717e7aa48bc96b268868d73dd6d2bd49
piecharton <46318475+piecharton@users.noreply.github.com>
2025-01-15 23:14:50 +0100

    argument parsing on server
    Should be advanced enough
    + isolated in fct

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -8,48 +8,106 @@
 #include <argp.h>
 #include <vector>
 
-// TODO Parse arguments into structure
+// DONE Parse arguments into structure
 
-int main(int argc, char *argv[])
+struct game_config
+{
+    bool validity = true;
+    int port = 0;
+    int width = 100;
+    int height = 100;
+    int time = 1;
+    std::vector<std::string> team_names{"Blazing pandas",
+                                        "Fruity postcards"};
+    int clients = 0;
+};
+
+std::ostream &operator<<(std::ostream &out, const struct game_config &obj)
+{
+    // write obj to stream
+    std::string team_names;
+    for (const std::string &name : obj.team_names)
+    {
+        team_names.append("\t");
+        team_names.append(name);
+        team_names.append("\n");
+    }
+
+    return out << "Server will listen to port " << obj.port << std::endl
+               << "Game width set to " << obj.width << std::endl
+               << "Game height set to " << obj.height << std::endl
+               << "Number of clients required to start the game " << obj.clients << std::endl
+               << "Game time unit is 1/" << obj.time << std::endl
+               << "Teams are " << std::endl
+               << team_names;
+}
+
+struct game_config argument_parsing(int argc, char *argv[])
 {
     int opt;
     std::vector<std::string> team_names;
 
+    struct game_config config;
+
     while ((opt = getopt(argc, argv, "p:x:y:n:c:t:")) != -1)
     {
-        switch (opt)
+        try
         {
-        case 'p':
-        case 'x':
-        case 'y':
-        case 'c':
-        case 't':
-        {
-            const std::string argument_option(optarg, strlen(optarg));
-            std::cout << "option [" << static_cast<char>(opt) << "] with arg " << argument_option << std::endl;
-        }
-        break;
-        case 'n':
-        {
-            //team names parsing
-            team_names.push_back(std::string(optarg, strlen(optarg)));
-                std::cout << team_names.back() << std::endl;
-
-            for (int i = optind; (i < argc) && (argv[i][0] != '-'); ++i)
+            switch (opt)
             {
-                const std::string current_team_name(argv[i], strlen(argv[i]));
-                team_names.push_back(current_team_name);
-                std::cout << team_names.back() << std::endl;
+            // only numbers
+            case 'p':
+                config.port = std::stoi(std::string(optarg, strlen(optarg)));
+                break;
+            case 'x':
+                config.width = std::stoi(std::string(optarg, strlen(optarg)));
+                break;
+            case 'y':
+                config.height = std::stoi(std::string(optarg, strlen(optarg)));
+                break;
+            case 'c':
+                config.clients = std::stoi(std::string(optarg, strlen(optarg)));
+                break;
+            case 't':
+                config.time = std::stoi(std::string(optarg, strlen(optarg)));
+                break;
+            case 'n':
+            {
+                // team names parsing
+                config.team_names.clear();
+                config.team_names.push_back(std::string(optarg, strlen(optarg)));
+
+                for (int i = optind; (i < argc) && (argv[i][0] != '-'); ++i)
+                {
+                    config.team_names.push_back(std::string(argv[i], strlen(argv[i])));
+                }
+            }
+            break;
+            case '?':
+            default:
+                std::cout << "Unknown option:" << optopt << std::endl;
+                break;
             }
         }
-        break;
-        case '?':
-        default:
-            std::cout << "Unknown option:" << optopt << std::endl;
-            break;
+        catch (const std::exception &e)
+        {
+            std::cerr << "Exception caught : " << e.what() << '\n';
+            config.validity = false;
         }
     }
 
+    std::cout << config;
+    return config;
+}
+
+int main(int argc, char *argv[])
+{
+
+    struct game_config config = argument_parsing(argc, argv);
+    if (config.validity)
+    {
+        std::cout << "config valid" << std::endl;
+    }
     // creating socket
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
